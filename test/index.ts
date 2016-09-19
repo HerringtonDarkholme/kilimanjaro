d90f86ba2edd272a39f982cef7e135ccba5a2ed1
Herrington Darkholme <nonamesheep1@gmail.com>
2016-09-19 20:01:20 +0800

    implement module basic test

--- test/index.ts
+++ test/index.ts
@@ -144,4 +144,84 @@ describe('Kilimanjaro', () => {
     dispatch('b')()
     expect(b).to.have.been.called
   })
+
+  it('module: mutation', () => {
+    const mutation = (s: {a: number}) => (n: number) => {
+      s.a += n
+    }
+
+    const store = create({a: 1})
+      .mutation(TEST, mutation)
+      .module('nested', create({a: 2})
+        .mutation(TEST, mutation)
+        .module('one', create({a: 3})
+          .mutation(TEST, mutation))
+        .module('nested', create()
+          .module('two', create({a: 4})
+            .mutation(TEST, mutation))
+          .module('three', create({a: 5})
+            .mutation(TEST, mutation))))
+      .module('four', create({a: 6})
+        .mutation(TEST, mutation))
+      .done()
+    store.commit(TEST)(1)
+    expect(store.state.a).to.equal(2)
+    expect(store.state.$('nested').a).to.equal(3)
+    expect(store.state.$('nested').$('one').a).to.equal(4)
+    expect(store.state.$('nested').$('nested').$('two').a).to.equal(5)
+    expect(store.state.$('nested').$('nested').$('three').a).to.equal(6)
+    expect(store.state.$('four').a).to.equal(7)
+  })
+
+  it('module: action', () => {
+    let calls = 0
+    const makeAction = (n: number) => ({state}: {state: {a: number}}) => () => {
+      calls++
+      expect(state.a).to.equal(n)
+    }
+    const store = create({a: 1})
+      .action(TEST, makeAction(1))
+      .module('nested', create({a: 2})
+        .action(TEST, makeAction(2))
+        .module('one', create({a: 3})
+          .action(TEST, makeAction(3)))
+        .module('nested', create()
+          .module('two', create({a: 4})
+            .action(TEST, makeAction(4)))
+          .module('three', create({a: 5})
+            .action(TEST, makeAction(5)))))
+      .module('four', create({a: 6})
+        .action(TEST, makeAction(6)))
+      .done()
+    store.dispatch(TEST)()
+    expect(calls).to.equal(6)
+  })
+
+
+  it('module: getters', () => {
+    const makeGetter = (n: number) => (s: any, g: any) => {
+      expect(g('contant')).to.equal(0)
+      expect(s.a).to.equal(n)
+      return s.a
+    }
+    const store = create({a: 1})
+      .getter('contant', _ => 0)
+      .getter('g1', makeGetter(1))
+      .module('nested', create({a: 2})
+        .getter('g2', makeGetter(2))
+        .module('one', create({a: 3})
+          .getter('g3', makeGetter(3)))
+        .module('nested', create()
+          .module('two', create({a: 4})
+            .getter('g4', makeGetter(4)))
+          .module('three', create({a: 5})
+            .getter('g5', makeGetter(5)))))
+      .module('four', create({a: 6})
+        .getter('g6', makeGetter(6)))
+      .done()
+
+      for (let i of [1, 2, 3, 4, 5, 6]) {
+        expect(store.getters(`g${i}` as any)).to.equal(i)
+      }
+  })
 })
